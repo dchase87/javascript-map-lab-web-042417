419cd127d4bbd3f21010a11bb02f1387fe7f07fa
dchase87 <david.chase4@gmail.com>
2017-06-06 17:53:27 -0400

    Done.

--- index.js
+++ index.js
@@ -21,3 +21,13 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null }
 ];
+
+var sortedRobots = robots.map(function (robot) {
+  return Object.assign({}, robot, {alliance: knownDecepticons.includes(robot.name) ? 'decepticon' : 'autobot'})
+})
+
+const even = [0, 2, 4, 6, 8, 10]
+
+var coloredZebraStripes = zebraStripes.map(function (stripe) {
+  return Object.assign({}, stripe, {color: zebraStripes.indexOf(stripe) % 2 == 0 ? 'black' : 'white'})
+})
